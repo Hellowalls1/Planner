e0bc8564f5ef77970e99c5626f6204d4d6b28b4c
Hellowalls1 <thesingingbutcher@gmail.com>
2020-05-27 20:47:21 -0500

    city stuff

---
+++ City.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Planner {
+
+    public class City {
+
+        public string _cityName { get; set; }
+        public string Mayor { get; set; }
+
+        public int _yearEstablished { get; set; }
+
+        public int
+
+    }
+
+}
