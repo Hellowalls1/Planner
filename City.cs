16a086ff3dd10908e322d185a8b7f35f1105feca
Hellowalls1 <thesingingbutcher@gmail.com>
2020-05-28 15:54:53 -0500

    got some of the city stuff done

--- City.cs
+++ City.cs
@@ -1,15 +1,25 @@
 using System;
+using System.Collections.Generic;
 
+//everything is in the namespace so Building can be called in City.cs
 namespace Planner {
 
     public class City {
 
-        public string _cityName { get; set; }
+        public string Name { get; set; }
         public string Mayor { get; set; }
 
-        public int _yearEstablished { get; set; }
+        public int Year { get; set; }
 
-        public int
+        private List<Building> _listBuildings = new List<Building> ();
+
+        //the type first(Building) and then the new parameter name
+        //creating a method for adding a new building to _listBuildings 
+        public void AddBuilding (Building newBuilding)
+
+        {
+            _listBuildings.Add (newBuilding);
+        }
 
     }
 
--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@ namespace Planner {
     class Program {
         static void Main (string[] args) {
 
-            Building thespot = new Building ("512 8th Avenue") {
+            City Crawfordsville = new City () {
+
+                Name = "Crawfordsville",
+                Mayor = "Sam Stone",
+                Year = 1989
+
+            };
+
+            Building theSpot = new Building ("512 8th Avenue") {
 
                 //
 
@@ -14,10 +22,29 @@ namespace Planner {
 
             };
 
-            thespot.Construct ();
-            thespot.Purchase ("Grandpa");
+            Building theTallOne = new Building ("1008 Main St") {
 
-            Console.WriteLine (thespot);
+                Width = 200,
+                Depth = 2000,
+                Stories = 20
+            };
+
+            Building theShortOne = new Building ("2012 West 4th St.") {
+
+                Width = 200,
+                    Depth = 2000,
+                    Stories = 20
+
+            }
+
+            theSpot.Construct ();
+            theSpot.Purchase ("Grandpa");
+            theTallOne.Construct ();
+            theTallOne.Purchase ("Mom");
+            Console.WriteLine (theSpot);
+            Console.WriteLine (theTallOne);
+
+            Crawfordsville.AddBuilding (theSpot);
         }
     }
 
